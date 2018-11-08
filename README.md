f79fb503a23ca0484f8218fe9f88de7921004a02
Marble Wu <huobazi@gmail.com>
2018-11-08 19:04:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+```
+rails new back-end --api 
+```
+
+```
+vue create front-end
+```
+
+```
+cd front-end && npm run build
+```
+
+```
+cd back-end && powder link
+```
+
+```
+cd back-end && powder open
+```
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!--
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      -->
+    <ProductList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import ProductList from '@/components/ProductList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,ProductList
+  }
+}
+</script>
